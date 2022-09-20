3c96a7610cac029186446c5c6389b65344b02db9
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-09-20 10:44:38 -0300

    Adding a Page Config file

---
+++ src/utils/PageConfig.ts
@@ -0,0 +1,6 @@
+export const PageConfig = {
+  site_name: "string",
+  title: "Elias Pereyra Gomez | Fullstack, JavaScript, React",
+  description: "My personal website where you'll find all my recent projects and, also, a little bit of who I am",
+  author: "Elias Pereyra Gomez"
+}
