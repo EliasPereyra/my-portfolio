dee1c2f1bb832823e51e23e007852504e97bfe8d
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-09-22 23:20:04 -0300

    Removing react plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react()],
 });
