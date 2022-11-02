cf8f63e635e7d725c59f32f8d823a6c92584b5eb
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-11-01 21:48:52 -0300

    Solving a typo in the title of the page

--- src/utils/PageConfig.ts
+++ src/utils/PageConfig.ts
@@ -1,6 +1,6 @@
 export const PageConfig = {
   site_name: "string",
-  title: "Elias Pereyra Gomez | Fullstack, JavaScript, React",
+  title: "Elias Pereyra Gomez | Fullstack, JavaScript, React.js",
   description: "My personal website where you'll find all my recent projects and, also, a little bit of who I am",
   author: "Elias Pereyra Gomez"
 }
