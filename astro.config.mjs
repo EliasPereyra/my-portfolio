7285e83610e8cc5679c385c32c72ab89221d6a32
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-11-07 22:58:53 -0300

    Removing i18next regarding some issues with configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from '@astrojs/react';
 import netlify from "@astrojs/netlify/functions";
-import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react(), astroI18next()],
+  integrations: [react()],
 });
