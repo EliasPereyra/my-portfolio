5ea299c371b3d8ec46e163f5b8b0e0f47a5cc0c9
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-03-31 17:27:32 -0300

    Delete the site name property

--- src/utils/PageConfig.ts
+++ src/utils/PageConfig.ts
@@ -1,5 +1,4 @@
 export const PageConfig = {
-  site_name: "string",
   title: "Elias Pereyra Gomez | Fullstack, JavaScript, React.js",
   description: "My personal website where you'll find all my recent projects and, also, a little bit of who I am",
   author: "Elias Pereyra Gomez"
