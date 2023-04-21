9ca5bc3534bbd60530aef47ae75cc97a8de262c1
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-04-20 23:40:22 -0300

    Create a projects file containing all the data about each developed project

---
+++ src/utils/Projects.ts
@@ -0,0 +1,35 @@
+export interface Projects {
+  title: string;
+  desc: string;
+  project_img: string;
+  tags: string[];
+  live_link: string;
+  repo_link: string;
+}
+
+export const projects: Projects[] = [
+  {
+    title: "Expense Tracker",
+    desc: "An app designed for tracking different types of transactions: incomes and expenses. The project is built with React.js, using the Speechly AI library.",
+    project_img: "/assets/expense-tracker.webp",
+    tags: ["React.js ", "Material UI ", "Speechly AI"],
+    live_link: "https://s-expensetracker.netlify.app",
+    repo_link: "https://github.com/eliaspereyra/expense-tracker",
+  },
+  {
+    title: "Memories",
+    desc: "This app was made with the MERN stack. Allows you to share memories with the people, attaching an image. A memory is a post which has a title and a description of you like, altogether with a photo or an image.",
+    project_img: "/assets/memories-app.webp",
+    tags: ["React.js ", "Node.js ", "Express.js ", "MongoDB ", "Material UI"],
+    live_link: "https://imagery-app.netlify.app",
+    repo_link: "https://github.com/eliaspereyra/memories-project",
+  },
+  {
+    title: "Portfolio",
+    desc: "A portfolio made for a drawer and designer from Argentina. It was made with HTML, Sass and JavaScript.",
+    project_img: "/assets/yonni-art.webp",
+    tags: ["HTML", "Sass", "JavaScript"],
+    live_link: "https://www.yonni.art/",
+    repo_link: "https://github.com/EliasPereyra/yonni-art",
+  },
+]
