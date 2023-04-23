af7a48bc88f1ad6a30ff97d26056835990c19631
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-04-22 23:34:11 -0300

    Add types to the page config file

--- src/utils/PageConfig.ts
+++ src/utils/PageConfig.ts
@@ -1,4 +1,10 @@
-export const PageConfig = {
+export interface PageConfig {
+  title: string;
+  description: string;
+  author: string;
+}
+
+export const PageConfig: PageConfig = {
   title: "Elias Pereyra Gomez | Fullstack, JavaScript, React.js",
   description: "My personal website where you'll find all my recent projects and, also, a little bit of who I am",
   author: "Elias Pereyra Gomez"
