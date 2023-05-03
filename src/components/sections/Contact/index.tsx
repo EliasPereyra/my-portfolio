bd770d9b19258ed2d6ac409512aa8d66af9b553c
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-05-02 21:34:15 -0300

    Change the background color with a gradient one

--- src/components/sections/Contact/index.tsx
+++ src/components/sections/Contact/index.tsx
@@ -1,42 +1,19 @@
 import { t } from "i18next"
-import emailjs from "@emailjs/browser"
 
 import "./styles.css"
 
 function Contact() {
-  const templateParams = {
-    name: "Elias",
-    notes: "Check this out",
-  }
-
-  const send = emailjs
-    .send(
-      "service_iewzp4j",
-      "template_5d2x7k2",
-      templateParams,
-      "npZhM9ycdFWILFA3A"
-    )
-    .then(
-      (response) => {
-        console.log("Success!", response.status, response.text)
-        return response.text
-      },
-      (err) => {
-        console.log(err)
-      }
-    )
-
   return (
     <section id="contact">
       <div id="noise"></div>
       <div id="contact-container">
-      <div id="underlay"></div>
-        <div id="contact-container__right-side" class="animate">
+        <div id="underlay"></div>
+        <div id="contact-container__right-side" className="animate">
           <h2>{t("contact.title")}</h2>
           <p>{t("contact.comment")}</p>
         </div>
-        <div id="contact-container__content" class="animate">
-          <form action="mailto:eliaspereyra_gomez@hotmail.com" method="POST">
+        <div id="contact-container__content" className="animate">
+          <form method="POST">
             <label htmlFor="username">{t("contact.name")}</label>
             <input
               id="username"
@@ -59,7 +36,7 @@ function Contact() {
 
             <label htmlFor="message">{t("contact.message")}</label>
             <textarea id="message" name="message" rows={3} required></textarea>
-            <button type="submit" value="send" onClick={send}>
+            <button type="submit" value="send">
               {t("contact.send-button")}
             </button>
           </form>
