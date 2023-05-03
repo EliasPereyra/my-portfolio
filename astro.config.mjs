17a1dc13d806fc4225b3a17cb8761505bb422291
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-05-02 22:15:30 -0300

    Take the server option away

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   adapter: netlify(),
   integrations: [react(), astroI18next()],
 });
