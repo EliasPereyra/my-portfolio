ddbec64664c7773064b1bac8e34ff37eb8606472
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-07 19:33:45 -0300

    Feat: add colors to every tag

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -1,10 +1,24 @@
 import { t } from 'i18next'
 
+const reactColor = Symbol("#61DAFB")
+const materiaUIColor = Symbol("#007FFF")
+const rtkColor = Symbol("#764ABC")
+const antDesignColor = Symbol("#0170FE")
+const chartJsColor = Symbol("#FF6384")
+const htmlColor = Symbol("#E34F26")
+const sassColor = Symbol("#CC6699")
+const javaScriptColor = Symbol("#F7DF1E")
+
+export interface TagData {
+  tagName: string;
+  tagColor: string;
+}
+
 export interface Projects {
   title: string;
   desc: string;
   project_img: string;
-  tags: string[];
+  tags: TagData[];
   live_link: string;
   repo_link: string;
 }
@@ -14,7 +28,11 @@ export const projects: Projects[] = [
     title: "Expense Tracker",
     desc: `${t("projects.expense_tracker.desc")}`,
     project_img: "/assets/expense-tracker.webp",
-    tags: ["React.js ", "Material UI ", "Speechly AI"],
+    tags: [
+      { tagName: "React.js", tagColor: reactColor.description },
+      { tagName: "Material UI", tagColor: materiaUIColor.description },
+      { tagName: "Speechly AI", tagColor: "" },
+    ],
     live_link: "https://s-expensetracker.netlify.app",
     repo_link: "https://github.com/eliaspereyra/expense-tracker",
   },
@@ -22,7 +40,12 @@ export const projects: Projects[] = [
     title: "Digcoin News",
     desc: "A SPA app which displays the current price of the most known digital coins currently at the market, and also the latest news about the crypto world.",
     project_img: "/assets/digcoin.webp",
-    tags: ["React.js ", "Redux Toolkit", "Ant Design", "Chart.js"],
+    tags: [
+      { tagName: "React.js", tagColor: reactColor.description },
+      { tagName: "RTK", tagColor: rtkColor.description },
+      { tagName: "Ant Design", tagColor: antDesignColor.description },
+      { tagName: "Chart.js", tagColor: chartJsColor.description },
+    ],
     live_link: "https://digcoinnews.netlify.app",
     repo_link: "https://github.com/eliaspereyra/cryptocurrency-news",
   },
@@ -30,7 +53,11 @@ export const projects: Projects[] = [
     title: "yonni.art",
     desc: "A portfolio made for a drawer and designer from Argentina. It was made with HTML, Sass and JavaScript.",
     project_img: "/assets/yonni-art.webp",
-    tags: ["HTML", "Sass", "JavaScript"],
+    tags: [
+      { tagName: "HTML", tagColor: htmlColor.description },
+      { tagName: "Sass", tagColor: sassColor.description },
+      { tagName: "JavaScript", tagColor: javaScriptColor.description },
+    ],
     live_link: "https://www.yonni.art/",
     repo_link: "https://github.com/EliasPereyra/yonni-art",
   },
