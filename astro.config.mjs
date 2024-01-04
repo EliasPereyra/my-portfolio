d505c37b9d5c9e90d719a0d8ec7adcf283740579
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2024-01-04 19:04:06 -0300

    Remove the react integration plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import react from '@astrojs/react';
 import netlify from "@astrojs/netlify/functions";
 import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://eliaspereyra.netlify.app",
   adapter: netlify(),
-  integrations: [react(), astroI18next()],
+  integrations: [astroI18next()],
 });
