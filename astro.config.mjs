5b45e14d059144b5631cfe457bf564e48a641aa4
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-04-17 13:12:27 -0300

    Add the config for tina cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,31 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
-import astroI18next from 'astro-i18next'
+import mdx from "@astrojs/mdx";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eliaspereyra.netlify.app",
   adapter: netlify(),
-  integrations: [astroI18next()],
+  markdown: {
+    drafts: true,
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
+  integrations: [
+    astroI18next(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        experimentalThemes: {
+          light: "github-dark",
+          dark: "github-dark",
+        },
+        wrap: true,
+      },
+      drafts: true,
+    }),
+  ],
 });
