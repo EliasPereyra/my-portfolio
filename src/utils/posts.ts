7c7e8ceb744e57949b501a1c44da51db7f35fcf4
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-04-28 22:45:34 -0300

    Create a func which returns all posts by category

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,7 +1,19 @@
-import { getCollection, type CollectionEntry } from "astro:content"
+import { getCollection, type CollectionEntry } from "astro:content";
 
 export const getPosts = async () => {
-  const result = await getCollection("posts")  
+  const result = await getCollection("posts");
 
-  return result 
-  }
+  return result;
+};
+
+export const getPostsByCategory = async () => {
+  const allPosts = await getCollection("posts");
+
+  return allPosts.map((post) => post.data);
+};
+
+const searchPostsByCategory = async (tag: string) => {
+  const allPosts = await getCollection("posts");
+
+  return allPosts.filter((post) => post.data.tags.includes(tag));
+};
