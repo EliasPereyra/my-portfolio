5cd89c153d475de75e22d80fe79fc6b9a788f1c4
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-23 15:16:58 -0300

    Deactivate sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
-import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -29,13 +28,6 @@ export default defineConfig({
         wrap: true,
       },
       drafts: true,
-    }),
-    sentry({
-      dsn: "https://b9fbbc7d0c5b527593a49e8b4e96b8f2@o4507285139947520.ingest.us.sentry.io/4507285142700032",
-      sourceMapsUploadOptions: {
-        project: "javascript-astro",
-        authToken: import.meta.SENTRY_AUTH_TOKEN,
-      },
     }),
   ],
 });
