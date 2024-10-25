5e165f45b76eb5b247b70a14c29498a782ef2f50
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-25 18:02:12 -0300

    Delete a comment line

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import netlify from "@astrojs/netlify/functions";
 import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
 
-// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   site: "https://eliaspereyra.netlify.app",
