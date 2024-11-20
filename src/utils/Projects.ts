80e47a23688694a315bdf393c8281cfac693e92e
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-11-20 00:53:08 -0300

    Update the new projects by adding icons for each tech

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -1,7 +1,8 @@
-import { axiosColor, dotNetColor, expressjsColor, postgreSqlColor, reactColor, reactHookFormColor, reactRouterColor, sequelizeColor, sqlServerColor, tailwindCssColor, typescriptColor } from "./TechColors";
+import { techColors } from "./TechColors";
 
 export interface TagData {
   tagName: string;
+  tagIcon: string;
   tagColor: string | undefined;
 }
 
@@ -16,37 +17,57 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title:"Red Comunidad", 
+    title: "Turnos Doctor",
     desc: "Red Comunidad es una Web de reclamos vecinales y ciudadanos, donde se podrá reportar y visibilizar problemáticas ciudadanas y/o de infraestructura comunes a todos.",
-    project_img: "/assets/redco.webp",
+    project_img: "/assets/turnos-doctor.jpg",
     tags: [
-      { tagName: "React.js", tagColor: reactColor.description },
-      { tagName: "TailwindCSS", tagColor: tailwindCssColor.description },
-      { tagName: "Axios", tagColor: axiosColor.description },
-      { tagName: "React Hook Form", tagColor: reactHookFormColor.description },
-      { tagName: "Cloudinary", tagColor: reactColor.description },
-      { tagName: ".Net", tagColor: dotNetColor.description },
-      { tagName: "SQL Server", tagColor: sqlServerColor.description },
+      {
+        tagName: "React.js",
+        tagIcon: "/assets/icons/react.svg",
+        tagColor: techColors.React.description,
+      },
+      {
+        tagName: "Next.js",
+        tagIcon: "/assets/icons/nextdotjs.svg",
+        tagColor: techColors.NextJs.description,
+      },
+      {
+        tagName: "Shadcn",
+        tagIcon: "/assets/icons/shadcnui.svg",
+        tagColor: techColors.ShadcnUI.description,
+      },
+      {
+        tagName: "TailwindCSS",
+        tagIcon: "/assets/icons/tailwindcss.svg",
+        tagColor: techColors.TailwindCSS.description,
+      },
+      {
+        tagName: "Appwrite",
+        tagIcon: "/assets/icons/appwrite.svg",
+        tagColor: techColors.Appwrite.color.description,
+      },
+      {
+        tagName: "React Hook Form",
+        tagIcon: "/assets/icons/reacthookform.svg",
+        tagColor: techColors.reactHookForm.description,
+      },
+      {
+        tagName: "React Table",
+        tagIcon: "/assets/icons/reacttable.svg",
+        tagColor: techColors.ReactTable.description,
+      },
+      {
+        tagName: "Zod",
+        tagIcon: "/assets/icons/zod.svg",
+        tagColor: techColors.DotNet.description,
+      },
+      {
+        tagName: "TypeScript",
+        tagIcon: "/assets/icons/typescript.svg",
+        tagColor: techColors.TS.description,
+      },
     ],
-    live_link: "https://s10nc.somee.com/",
-    repo_link: "https://github.com/No-Country/s10-20-t-csharp-react"
+    live_link: "",
+    repo_link: "https://github.com/EliasPereyra/docheal",
   },
-  {
-    title: "Multishop",
-    desc: "Este es una aplicación fullstack de un e-commerce multi-cliente donde tantos vendedores pueden publicar y vender sus productos como los clientes pueden encontrar productos de todo tipo.",
-    project_img: "/assets/multishop.webp",
-    tags: [
-      { tagName: "React.js", tagColor: reactColor.description },
-      { tagName: "TypeScript", tagColor: typescriptColor.description },
-      { tagName: "React Router", tagColor: reactRouterColor.description },
-      { tagName: "Zustand", tagColor: reactColor.description },
-      { tagName: "Axios", tagColor: axiosColor.description },
-      { tagName: "TailwindCSS", tagColor: tailwindCssColor.description },
-      { tagName: "Express.js", tagColor: expressjsColor.description },
-      { tagName: "Sequelize", tagColor: sequelizeColor.description },
-      { tagName: "PostgreSQL", tagColor: postgreSqlColor.description },
-    ],
-    live_link: "https://c12-28-t-node-react-wu3w.vercel.app/",
-    repo_link: "https://github.com/No-Country/c12-28-t-node-react"
-  }
-]
+];
